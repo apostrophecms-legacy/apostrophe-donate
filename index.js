c88a1fbde9d3ea450d1c26e201e0c78d461c5367
livhaas <olivia@punkave.com>
2014-07-18 15:21:12 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,320 @@
+var paypal_sdk = require('paypal-rest-sdk');
+var countries = require('./countries.js');
+var _ = require('lodash');
+var async = require('async');
+var csv = require('csv');
+var moment = require('moment');
+
+module.exports = factory;
+
+function factory(options, callback) {
+  return new Construct(options, callback);
+}
+
+function Construct(options, callback) {
+  var self = this;
+  var app = options.app;
+
+  paypal_sdk.configure(options.payPal);
+
+  self._apos = options.apos;
+  self._apos.mixinModuleAssets(self, 'donate', __dirname, options);
+  self._apos.mixinModuleEmail(self);
+  self._schemas = options.schemas;
+
+  options.addFields = [
+    {
+      name: 'total',
+      label: 'Amount',
+      type: 'string',
+      required: true
+    },
+    {
+      name: 'first_name',
+      label: 'First Name',
+      type: 'string',
+      required: true
+    },
+    {
+      name: 'last_name',
+      label: 'Last Name',
+      type: 'string',
+      required: true
+    },
+    {
+      name: 'type',
+      label: 'Credit Card Type',
+      type: 'select',
+      required: true,
+      choices: [
+        {
+          value: null,
+          label: 'CHOOSE ONE'
+        },
+       {
+         value: 'visa',
+         label: 'Visa'
+       },
+       {
+         value: 'mastercard',
+         label: 'Mastercard'
+       },
+       {
+         value: 'discover',
+         label: 'Discover'
+       },
+       {
+         value: 'amex',
+         label: 'American Express'
+       }
+      ]
+    },
+    {
+      name: 'number',
+      label: 'Credit Card Number',
+      type: 'string',
+      required: true,
+    },
+    {
+      name: 'cvv2',
+      label: 'CVV2',
+      type: 'integer',
+      required: true
+    },
+    {
+      name: 'expire_month',
+      label: 'Expire Month',
+      type: 'integer',
+      required: true,
+    },
+    {
+      name: 'expire_year',
+      label: 'Expire Year',
+      type: 'integer',
+      required: true,
+    },
+    {
+      name: 'line1',
+      label: 'Street Address',
+      type: 'string',
+      required: true
+    },
+    {
+      name: 'city',
+      label: 'City',
+      type: 'string',
+      required: true
+    },
+    {
+      name: 'state',
+      label: 'State',
+      type: 'string',
+      required: true
+    },
+    {
+      name: 'postal_code',
+      label: 'Zip Code',
+      type: 'string',
+      required: true
+    },
+    {
+      name: 'country_code',
+      label: 'Country',
+      type: 'select',
+      required: true,
+      choices: [ { label: 'CHOOSE ONE', value: undefined } ].concat(countries)
+    },
+    {
+      name: 'email',
+      label: 'Email Address',
+      type: 'string',
+      required: true
+    }
+  ].concat(options.addFields || []);
+
+  self.schema = self._schemas.compose(options);
+  // _.each(self.schema, function(item) {
+  //   console.log(item.label + ',' + item.type);
+  // });
+  self._apos.pushGlobalData({
+    aposDonate: {
+      schema: self.schema
+    }
+  });
+
+  // create a custom nunjucks function to render our form
+  self._apos.addLocal('aposDonationForm', function(){
+    return self.render('formWrapper', {countries: countries, schema: self.schema});
+  });
+
+  app.get('/apos-donate', function(req, res) {
+    return res.send(self.render('form', {countries: countries, schema: self.schema}));
+  });
+
+  app.post('/apos-donate', function(req, res) {
+    var errors = {};
+    var values = {};
+
+    return self._schemas.convertFields(req, self.schema, 'form', req.body, values, function(err) {
+
+      if (values.expire_year < 2000) {
+        values.expire_year = values.expire_year + 2000;
+      }
+
+      var payment_details = {
+        "intent": "sale",
+        "payer": {
+          "payment_method": "credit_card",
+          "funding_instruments": [{
+            "credit_card": {
+              "type": values.type,
+              "number": values.number,
+              "expire_month": values.expire_month,
+              "expire_year": values.expire_year,
+              "cvv2": values.cvv2,
+              "first_name": values.first_name,
+              "last_name": values.last_name,
+              "billing_address": {
+                "line1": values.line1,
+                "city": values.city,
+                "state": values.state,
+                "postal_code": values.postal_code,
+                "country_code": values.country_code }}}]},
+        "transactions": [{
+          "amount": {
+            "total": values.total,
+            "currency": "USD",
+            "details": {}
+            },
+          "description": "Donation to Better Tomorrows" }]};
+
+      return paypal_sdk.payment.create(payment_details, function(error, payment){
+        if(error){
+          console.error(error);
+          // console.error(JSON.stringify(error));
+          errors.number = "Your credit card was not accepted. Please double check your information.";
+          return send();
+        } else {
+          // console.log(values);
+          return async.series({
+            storeInDatabase: function(callback) {
+              var donation = _.cloneDeep(values);
+              donation.when = new Date();
+              delete donation.cvv2;
+              delete donation.expire_month;
+              delete donation.expire_year;
+              donation._id = 'd' + self._apos.generateId();
+              donation.number = donation.number.substr(donation.length -  4, 4);
+              self._donations.insert(donation, callback);
+            },
+            emailDonor: function(callback) {
+              // console.log('sent thank you email');
+              return self.email(
+                req,
+                {
+                  email: options.from.email,
+                  fullName: options.from.name
+                },
+                {
+                  email: values.email,
+                  fullName: values.first_name + ' ' + values.last_name
+                },
+                options.thankYouSubject || 'Thank you for your donation',
+                'thankYouEmail',
+                values,
+                callback
+              );
+            },
+            emailRecipient: function(callback){
+              return self.email(
+                req,
+                {
+                  email: options.from.email,
+                  fullName: options.from.name
+                },
+                {
+                  email:  options.recipient.email,
+                  fullName: options.recipient.name
+                },
+                options.confirmationSubject || 'Donation Received',
+                'recipientEmail',
+                values,
+                callback
+              );
+            }
+          },
+          function(err) {
+            if (err) {
+              console.log(err);
+            }
+            return send();
+          });
+        }
+      });
+    });
+    return send();
+
+    function send() {
+      return res.send({
+        status: _.isEmpty(errors) ? 'ok' : 'error',
+        errors: errors
+      });
+    }
+
+  });
+
+  app.get('/apos-export-donations', function(req, res) {
+    if (!self._apos.permissions.can(req, 'export-donations')) {
+      res.statusCode = 403;
+      return res.send('forbidden');
+    }
+    return self._donations.find({}).sort({ when: 1 }).toArray(function(err, donations) {
+      if (err) {
+        res.statusCode = 500;
+        return res.send('An error occurred');
+      }
+      res.setHeader('Content-disposition', 'attachment; filename=testing.csv');
+      res.setHeader('Content-type', 'text/csv');
+
+      // We have: an array of objects.
+      // We need: an array of rows, in which the first row has the heading
+      // names and the remainder have the data for one object apiece, in the
+      // same order as the headings.
+
+      var flat = [];
+      if (donations.length) {
+        var headings = _.keys(donations[0]);
+        flat = [ headings ];
+        _.each(donations, function(donation) {
+          flat.push(_.map(headings, function(heading) {
+            if (heading === 'when') {
+              // Raw Unix timestamps aren't very friendly
+              return moment(donation[heading]).format('YYYY-MM-DD');
+            } else {
+              return donation[heading];
+            }
+          }));
+        });
+      }
+      return csv.stringify(flat, function(err, csv) {
+        res.send(csv);
+      });
+    });
+  });
+
+  self._apos.mixinModuleAssets(self, 'donate', __dirname, options);
+  self.pushAsset('stylesheet', 'content', { when: 'always' });
+  self.pushAsset('script', 'content', { when: 'always' });
+
+  // Get hold of a MongoDB collection for donations
+  return self._apos.db.collection('donations', function(err, _donations) {
+    self._donations = _donations;
+    // Invoke the final callback. This must happen on next tick or later!
+    return process.nextTick(function() {
+      return callback(err);
+    });
+  });
+}
+
+// Export the constructor so others can subclass
+factory.Construct = Construct;
---
+++ public/js/content.js
@@ -0,0 +1,48 @@
+$(function() {
+  if ($('[data-apos-donate-form]').length) {
+
+    apos.requireScene('user', function(){
+      var schema = apos.data.aposDonate.schema;
+      var $el = $('[data-apos-donate-form]');
+      var $container = $('[data-apos-donate-form-container]');
+
+      var data = aposSchemas.newInstance(schema);
+
+      aposSchemas.populateFields($el, schema, data, function(){});
+
+      $('body').on('submit', '[data-apos-donate-form]', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        var data = {};
+        $el.find('.apos-donate-pending').addClass('apos-donate-show');
+        aposSchemas.convertFields($el, schema, data, function(error) {
+          if(error) {
+            $el.find('.apos-donate-pending').removeClass('apos-donate-show');
+            aposSchemas.scrollToError($el);
+            return;
+          }
+          // console.log(data)
+          $.post('/apos-donate', data, function(data){
+            if (data.status == 'ok') {
+              // console.log(data)
+              $('[data-apos-donate-form-thanks]').show();
+              $('[data-apos-donate-form]').remove();
+            } else {
+              //re-render form with errors
+              _.each(data.errors, function(value, key){
+                var $targetFieldset = aposSchemas.findFieldset($el, key);
+                var $error = $('<span data-apos-donate-error></span>');
+                $targetFieldset.find('[data-apos-donate-error]').remove();
+                $error.text(value);
+                $targetFieldset.append($error);
+              });
+            }
+            $el.find('.apos-donate-pending').removeClass('apos-donate-show');
+          });
+        });
+      });
+    });
+  }
+
+});
